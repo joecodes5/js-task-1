1ab473459e2d470ca163da4869ef11174cf931ac
joecodes5 <60202911+joecodes5@users.noreply.github.com>
2020-03-26 23:07:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const name = "joecodes5"
+const courses = ["HTML", "CSS", "HTML", "UI/UX"]
+
+console.log(name)
+console.log(courses)
+const numbersToDisplay = []
+for (let i=1; i<= 200; i++) {
+    if (i%2 == 0) {
+        numbersToDisplay.push(i)
+    }
+}
+console.log(numbersToDisplay)
